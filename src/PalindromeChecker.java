13cdb0d68d79556ae63cce0735a9c2c68585fc51
Nikhil <karurnikhil2507@gmail.com>
2026-03-11 10:23:37 +0530

    UC2 - Print a Hardcoded Palindrome Result

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -5,5 +5,22 @@ public class PalindromeChecker {
         System.out.println("   Version: 1.0                          ");
         System.out.println("=========================================");
         System.out.println("\nApplication is ready to process Data...");
+        
+        // UC2: Print a Hardcoded Palindrome Result
+        String word = "madam";
+        boolean isPalindrome = true;
+        
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        
+        if (isPalindrome) {
+            System.out.println("The word '" + word + "' is a palindrome.");
+        } else {
+            System.out.println("The word '" + word + "' is NOT a palindrome.");
+        }
     }
 }
