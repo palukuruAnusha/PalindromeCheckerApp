6b29136036dcde913dfbe7650f3d9d503478deb8
Nikhil <karurnikhil2507@gmail.com>
2026-03-11 10:23:11 +0530

    UC1 - Application Entry & Welcome Message

---
+++ src/PalindromeChecker.java
@@ -0,0 +1,9 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        System.out.println("=========================================");
+        System.out.println("   Welcome to PalindromeChecker App      ");
+        System.out.println("   Version: 1.0                          ");
+        System.out.println("=========================================");
+        System.out.println("\nApplication is ready to process Data...");
+    }
+}
